c3178b62e2167b53271d67acf5df18558d7f219f
Kleber <kleber_master@hotmail.com>
2021-02-06 19:51:52 -0300

    lista de dados da agenda

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+
 
 # Create your models here.
 class Evento(models.Model):
@@ -17,3 +19,8 @@ class Evento(models.Model):
         return self.data_evento.strftime('%d/%m/%Y %H:%M')
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+    def get_data_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
